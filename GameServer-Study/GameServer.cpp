46eea50e3dc43e353c14c3e362b67fb395723d6e
namu <guyus5594@gmail.com>
2023-05-10 10:09:16 +0900

    [멀티쓰레드 프로그래밍] : 캐시

--- GameServer-Study/GameServer.cpp
+++ GameServer-Study/GameServer.cpp
@@ -5,96 +5,56 @@
 #include <atomic>
 #include <mutex>
 #include <future>
+#include <windows.h>
 
-int64 result;
-
-int64 Calculate()
-{
-	int64 sum = 0;
-
-	for (int32 i = 0; i < 100'000; i++)
-	{
-		sum += i;
-	}
-
-	result = sum;
-
-	return sum;
-}
-
-void PromiseWorker(std::promise<string>&& promise)
-{
-	promise.set_value("비밀 메시지");
-}
-
-void TaskWorker(std::packaged_task<int64(void)>&& task)
-{
-	task();
-}
+// 2차원 배열을 사용하여 캐시에 대한 속도 차이를 확인하는 코드입니다.
+int32 buffer[10000][10000];
 
 int main()
 {
-	// 동기 (synchronous) 실행 예제
-	// int64 sum = Calculate();
-	// cout << sum << '\n';
+    memset(buffer, 0, sizeof(buffer));
 
-	// std::future 사용법
-	{
-		// 1) deferred		   -> 지연 평가 (lazy evaluation)
-		// 2) async			   -> 별도의 쓰레드에서 실행
-		// 3) deferred | async -> 두 가지 중 적절한 방법 선택
+    // 행 우선 순회 방식으로 배열을 순회하며 합을 구합니다.
+    // 이 방식은 캐시 메모리의 공간 지역성(Spatial Locality) 원리에 따라
+    // 배열이 메모리에 연속적으로 저장되므로 캐시 효율성이 높습니다.
+    {
+        uint64 start = GetTickCount64(); // 시작 시간 기록
 
-		// 언젠가 미래에 결과를 반환하는 객체 생성
-		std::future<int64> future = std::async(std::launch::async, Calculate);
+        int64 sum = 0;
 
-		// TODO
-		//std::future_status status = future.wait_for(1ms);
+        for (int32 i = 0; i < 10000; i++)
+        {
+            for (int32 j = 0; j < 10000; j++)
+            {
+                sum += buffer[i][j];
+            }
+        }
 
-		int64 sum = future.get(); // 여기서 결과를 가져옴
+        uint64 end = GetTickCount64(); // 종료 시간 기록
+        cout << "Elapsed Tick " << (end - start) << '\n'; // 경과 시간 출력
+    }
 
-		/*class Knight // 객체를 사용하는 경우 예제
-		{
-		public:
-			int64 GetHp() { return 100; }
-		};
+    // 열 우선 순회 방식으로 배열을 순회하며 합을 구합니다.
+    // 이 방식은 캐시 메모리의 공간 지역성 원리에 부합하지 않아
+    // 캐시 미스가 더 자주 발생하며, 따라서 속도가 느려집니다.
+    {
+        uint64 start = GetTickCount64(); // 시작 시간 기록
 
-		Knight knight;
-		std::future<int64> future2 = std::async(std::launch::async, &Knight::GetHp, knight);*/
-	}
+        int64 sum = 0;
+        for (int32 i = 0; i < 10000; i++)
+        {
+            for (int32 j = 0; j < 10000; j++)
+            {
+                sum += buffer[j][i];
+            }
+        }
 
-	// std::promise 사용법
-	{
-		// 미래(std::future)에 결과를 반환할 것이라는 약속(std::promise) 생성 (계약서 느낌)
-		std::promise<string> promise;
-		std::future<string> future = promise.get_future();
-
-		thread t(PromiseWorker, std::move(promise));
-
-		string message = future.get();
-		cout << message << '\n';
-
-		t.join();
-	}
-
-	// std::packaged_task 사용법
-	{
-		std::packaged_task<int64(void)> task(Calculate);
-		std::future<int64> future = task.get_future();
-
-		std::thread t(TaskWorker, std::move(task));
-
-		int64 sum = future.get();
-		cout << sum << '\n';
-
-		t.join();
-	}
-
-	// 결론)
-	// mutex, condition_variable까지 사용하지 않고, 간단한 작업을 처리할 수 있다.
-	// 특히, 한 번 발생하는 이벤트에 유용하다.
-
-	// 1) async			-> 원하는 함수를 비동기적으로 실행
-	// 2) promise		-> 결과물을 promise를 통해 future로 받아줌
-	// 3) packaged_task -> 원하는 함수의 실행 결과를 packaged_task를 통해 future로 받아줌
+        uint64 end = GetTickCount64(); // 종료 시간 기록
+        cout << "Elapsed Tick " << (end - start) << '\n'; // 경과 시간 출력
+    }
 }
 
+/* 이 코드에서 행 우선 방식이 더 빠른 이유는 캐시 메모리의 공간 지역성(Spatial Locality) 원리 때문입니다.
+공간 지역성 원리는 프로그램이 메모리에 인접한 위치에 있는 데이터를 참조할 확률이 높다는 것을 의미합니다.
+행 우선 방식은 메모리에 연속적으로 저장된 데이터를 순차적으로 접근하기 때문에 캐시 효율성이 높습니다.
+반면, 열 우선 방식은 메모리 접근이 불연속적이므로 캐시 미스가 더 자주 발생하고 속도가 느려집니다. */
