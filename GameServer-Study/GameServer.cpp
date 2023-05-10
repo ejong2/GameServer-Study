f2d0492b0788b34bdea23870e3c59f194e93453c
enamu <guyus5594@gmail.com>
2023-05-10 21:47:16 +0900

    [멀티쓰레드 프로그래밍] : 메모리 모델

--- GameServer-Study/GameServer.cpp
+++ GameServer-Study/GameServer.cpp
@@ -7,68 +7,110 @@
 #include <windows.h>
 #include <future>
 
-// 가시성, 코드 재배치
-int32 x = 0;
-int32 y = 0;
-int32 r1 = 0;
-int32 r2 = 0;
+// 메모리 정책
 
-volatile bool ready;
+atomic<bool> flag;
+atomic<bool> ready;
+int32 value;
 
-// Thread_1 함수에서는 변수 y에 1을 할당하고, r1에 변수 x의 값을 할당합니다.
-void Thread_1()
+void Producer()
 {
-	while (!ready)
-		;
+	value = 10;
 
-	y = 1;
-	r1 = x;
+	ready.store(true, memory_order::memory_order_seq_cst);
+
+	std::atomic_thread_fence(memory_order::memory_order_release);
+	// ready.store(true, memory_order::memory_order_release);
+	// ----------------- 절취선 -----------------
 }
 
-// Thread_2 함수에서는 변수 x에 1을 할당하고, r2에 변수 y의 값을 할당합니다.
-void Thread_2()
+void Consumer()
 {
-	while (!ready)
+	while (ready.load(memory_order::memory_order_seq_cst) == false)
 		;
 
+	// ----------------- 절취선 -----------------
+	// while (ready.load(memory_order::memory_order_acquire) == false);
+		
 
-	x = 1;
-	r2 = y;
+	cout << value << '\n';
 }
 
 int main()
 {
-	int32 count = 0;
+	ready = false;
+	value = 0;
+	thread t1(Producer);
+	thread t2(Consumer);
 
-	while (true)
-	{
-		ready = false;
+	t1.join();
+	t2.join();
 
-		count++;
+	// Memory Model (정책)
+	// 1) Sequentially Consistent (seq_cst)
+	// 2) Acquire-Release (acquire, release)
+	// 3) Relaxed (relaxed)
 
-		x = y = r1 = r2 = 0;
+	// 1) seq_cst (가장 엄격 = 컴파일러 최적화 여지 적음 = 직관적)
+	// 가시성 문제해결, 코드 재배치 문제 해결
+	
+	// 2) acquire-release
+	// 딱 중간 !
+	// release 명령 이전의 메모리 명령들이, 해당 명령 이후로 재배치 되는 것을 금지
+	// 그리고 acquire로 같은 변수를 읽는 쓰레드가 있다면 -> release 이전의 명령들이 -> acquire 하는 순간에 관찰 가능 (가시성)
 
-		// Thread_1과 Thread_2를 실행합니다.
-		thread t1(Thread_1);
-		thread t2(Thread_2);
+	// 3) relaxed (자유롭다 = 컴파일러 최적화 여지 많음 = 직관적이지 않음)
+	// 너무나도 자유롭다 !
+	// 코드 재배치도 멋대로 가능! 가시성 해결 NOT
+	// 가장 기본 조건 (동일 객체에 대한 동일 관전 순서만 보장)
 
-		ready = true;
-
-		t1.join();
-		t2.join();
-
-		// r1과 r2 모두 0인 경우, 즉 CPU 파이프라인의 최적화로 인한 메모리 순서 재배치가 발생한 경우를 확인합니다.
-		if (r1 == 0 && r2 == 0)
-		{
-			break;
-		}
-	}
-
-	// 해당 상황이 발생한 횟수를 출력합니다.
-	cout << count << " 번만에 빠져나옴." << '\n';
+	// 인텔, AMD의 경우 애당초 순차척 일관성을 보장을 해서,
+	// seq_cst를 써도 별다른 부하가 없음
+	// ARM의 경우 꽤 차이가 있다는 얘기가 있음
 }
 
-/* 이 코드는 CPU 파이프라인 최적화로 인해 메모리 순서가 재배치되는 상황을 보여줍니다.
-코드에서 두 스레드가 동시에 실행되는데, 각 스레드에서 서로 다른 변수에 1을 할당하고,
-서로 반대 변수의 값을 읽어옵니다. 이상적인 상황에서는 r1과 r2가 동시에 0이 될 수 없습니다.
-하지만 CPU 파이프라인 최적화로 인해 메모리 순서가 재배치되어 의도치 않은 결과가 발생할 수 있습니다. 이를 통해 파이프라인 최적화의 영향을 확인할 수 있습니다. */
+int main_test()
+{
+	flag = false;
+
+	//flag = true;
+	flag.store(true);
+
+	//bool val = flag;
+	bool val = flag.load(memory_order::memory_order_seq_cst);
+
+	// 이전 flag 값을 prev에 넣고, flag 값을 수정
+	{
+		bool prev = flag.exchange(true);
+		//bool prev = flag;
+		//flag = true;
+	}
+
+	// CAS (Compare-And-Swap) 조건부 수정
+	{
+		bool expected = false;
+		bool desired = true;
+		flag.compare_exchange_strong(expected, desired);
+
+		// Spurious Failure
+		if (flag == expected)
+		{
+			// 다른 쓰레드의 Interruption을 받아서 중간에 실패할 수 있음
+			//expected = flag;
+			flag = desired;
+			return true;
+		}
+		else
+		{
+			expected = flag;
+			return false;
+		}
+
+		while (true)
+		{
+			bool expected = false;
+			bool desired = true;
+			flag.compare_exchange_weak(expected, desired);
+		}
+	}
+}
