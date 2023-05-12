2efe91314d13e2a9b9dfd9c9bdf2e858bc82d41c
namu <guyus5594@gmail.com>
2023-05-12 10:06:14 +0900

    [멀티쓰레드 프로그래밍] : Thread Local Storage

--- GameServer-Study/GameServer.cpp
+++ GameServer-Study/GameServer.cpp
@@ -7,110 +7,43 @@
 #include <windows.h>
 #include <future>
 
-// 메모리 정책
+// 각 스레드에서 고유하게 사용되는 thread_local 변수 LThreadId를 선언합니다.
+thread_local int32 LThreadId = 0;
 
-atomic<bool> flag;
-atomic<bool> ready;
-int32 value;
-
-void Producer()
+// 스레드의 주요 작업을 수행하는 ThreadMain 함수입니다.
+void ThreadMain(int32 threadId)
 {
-	value = 10;
+    // 각 스레드에게 전달된 threadId 값을 thread_local 변수 LThreadId에 할당합니다.
+    LThreadId = threadId;
 
-	ready.store(true, memory_order::memory_order_seq_cst);
-
-	std::atomic_thread_fence(memory_order::memory_order_release);
-	// ready.store(true, memory_order::memory_order_release);
-	// ----------------- 절취선 -----------------
-}
-
-void Consumer()
-{
-	while (ready.load(memory_order::memory_order_seq_cst) == false)
-		;
-
-	// ----------------- 절취선 -----------------
-	// while (ready.load(memory_order::memory_order_acquire) == false);
-		
-
-	cout << value << '\n';
+    // 무한 루프를 통해 각 스레드가 자신의 고유한 ID를 출력합니다.
+    while (true)
+    {
+        cout << "Hi ! I am Thread " << LThreadId << '\n';
+        this_thread::sleep_for(1s);
+    }
 }
 
 int main()
 {
-	ready = false;
-	value = 0;
-	thread t1(Producer);
-	thread t2(Consumer);
+    // 스레드 객체들을 저장할 벡터를 선언합니다.
+    vector<thread> threads;
 
-	t1.join();
-	t2.join();
+    // 10개의 스레드를 생성하고, 각 스레드에 고유한 threadId를 전달합니다.
+    for (int32 i = 0; i < 10; i++)
+    {
+        int32 threadId = i + 1;
+        threads.push_back(thread(ThreadMain, threadId));
+    }
 
-	// Memory Model (정책)
-	// 1) Sequentially Consistent (seq_cst)
-	// 2) Acquire-Release (acquire, release)
-	// 3) Relaxed (relaxed)
-
-	// 1) seq_cst (가장 엄격 = 컴파일러 최적화 여지 적음 = 직관적)
-	// 가시성 문제해결, 코드 재배치 문제 해결
-	
-	// 2) acquire-release
-	// 딱 중간 !
-	// release 명령 이전의 메모리 명령들이, 해당 명령 이후로 재배치 되는 것을 금지
-	// 그리고 acquire로 같은 변수를 읽는 쓰레드가 있다면 -> release 이전의 명령들이 -> acquire 하는 순간에 관찰 가능 (가시성)
-
-	// 3) relaxed (자유롭다 = 컴파일러 최적화 여지 많음 = 직관적이지 않음)
-	// 너무나도 자유롭다 !
-	// 코드 재배치도 멋대로 가능! 가시성 해결 NOT
-	// 가장 기본 조건 (동일 객체에 대한 동일 관전 순서만 보장)
-
-	// 인텔, AMD의 경우 애당초 순차척 일관성을 보장을 해서,
-	// seq_cst를 써도 별다른 부하가 없음
-	// ARM의 경우 꽤 차이가 있다는 얘기가 있음
+    // 모든 스레드가 종료될 때까지 대기합니다.
+    for (thread& t : threads)
+    {
+        t.join();
+    }
 }
 
-int main_test()
-{
-	flag = false;
-
-	//flag = true;
-	flag.store(true);
-
-	//bool val = flag;
-	bool val = flag.load(memory_order::memory_order_seq_cst);
-
-	// 이전 flag 값을 prev에 넣고, flag 값을 수정
-	{
-		bool prev = flag.exchange(true);
-		//bool prev = flag;
-		//flag = true;
-	}
-
-	// CAS (Compare-And-Swap) 조건부 수정
-	{
-		bool expected = false;
-		bool desired = true;
-		flag.compare_exchange_strong(expected, desired);
-
-		// Spurious Failure
-		if (flag == expected)
-		{
-			// 다른 쓰레드의 Interruption을 받아서 중간에 실패할 수 있음
-			//expected = flag;
-			flag = desired;
-			return true;
-		}
-		else
-		{
-			expected = flag;
-			return false;
-		}
-
-		while (true)
-		{
-			bool expected = false;
-			bool desired = true;
-			flag.compare_exchange_weak(expected, desired);
-		}
-	}
-}
+/* 이 코드에서는 TLS(Thread Local Storage)를 사용하여 각 스레드가 고유한 데이터를 저장하고 사용할 수 있도록 했습니다.
+TLS를 사용하면 스레드 간에 데이터 충돌이 발생하지 않으며, 동기화 메커니즘을 사용하지 않고 안전하게 데이터에 접근할 수 있습니다.
+이로 인해 성능 저하가 발생하지 않습니다.
+TLS를 사용하지 않은 경우, 공유 메모리를 사용하여 데이터를 관리해야 합니다. 이 경우, 동시에 여러 스레드가 공유 메모리에 접근하려 할 때 동기화 메커니즘을 사용해야 하며, 이로 인해 성능 저하가 발생할 수 있습니다. */
