c215d146e5cc07219ab2f4482186c7024957254b
enamu <guyus5594@gmail.com>
2023-05-14 20:11:51 +0900

    [멀티쓰레드 프로그래밍] : Lock-Free Stack #2

--- GameServer-Study/ConcurrentStack.h
+++ GameServer-Study/ConcurrentStack.h
@@ -3,18 +3,16 @@
 template<typename T>
 class LockStack
 {
-	// 락을 사용하는 스택 클래스입니다.
 };
 
 template<typename T>
 class LockFreeStack
 {
-	// 락을 사용하지 않는 락-프리 스택 클래스입니다.
 	struct Node
 	{
-		Node(const T& value) : data(value)
+		Node(const T& value) : data(value), next(nullptr)
 		{
-			// 노드 생성자입니다. 전달된 값으로 data를 초기화합니다.
+			// 노드 생성자입니다. 전달된 값으로 data를 초기화하고, next를 nullptr로 초기화합니다.
 		}
 
 		T data;
@@ -25,44 +23,102 @@ public:
 
 	void Push(const T& value)
 	{
-		// 새 노드를 생성하고, 스택의 head에 추가합니다.
 		Node* node = new Node(value);
 		node->next = _head;
 
-		// compare_exchange_weak를 사용하여 head를 변경하는 작업이 원자적으로 수행되도록 합니다.
 		while (_head.compare_exchange_weak(node->next, node) == false)
 		{
 
 		}
 	}
 
-	// 1) head 읽기
-	// 2) head->next 읽기
-	// 3) head = head->next
-	// 4) data 추출해서 반환
-	// 5) 추출한 노드를 삭제
 	bool TryPop(T& value)
 	{
-		// 스택에서 노드를 추출하여 값을 반환하는 함수입니다.
+		++_popCount; // Pop을 실행하는 스레드의 개수를 증가시킵니다.
+
 		Node* oldHead = _head;
 
-		// compare_exchange_weak를 사용하여 head를 변경하는 작업이 원자적으로 수행되도록 합니다.
 		while (oldHead && _head.compare_exchange_weak(oldHead, oldHead->next) == false)
 		{
 
 		}
 
 		if (oldHead == nullptr)
-			return false; // 노드가 없는 경우, false를 반환합니다.
-
-		value = oldHead->data; // 노드의 값을 반환합니다.
-
+		{
+			--_popCount;
+			return false;
+		}
+		value = oldHead->data;
+		TryDelete(oldHead); // 추출한 노드를 삭제하려고 시도합니다.
 		return true;
 	}
+
+	void TryDelete(Node* oldHead)
+	{
+		if (_popCount == 1)
+		{
+			Node* node = _pendingList.exchange(nullptr);
+
+			if (--_popCount == 0)
+			{
+				DeleteNodes(node);
+			}
+			else
+			{
+				ChainPendingNodeList(node);
+			}
+
+			delete oldHead;
+		}
+		else
+		{
+			ChainPendingNode(oldHead);
+			--_popCount;
+		}
+	}
+
+	void ChainPendingNodeList(Node* first, Node* last)
+	{
+		last->next = _pendingList;
+
+		while (_pendingList.compare_exchange_weak(last->next, first) == false)
+		{
+
+		}
+	}
+
+	void ChainPendingNodeList(Node* node)
+	{
+		Node* last = node;
+		while (last->next)
+			last = last->next;
+
+		ChainPendingNodeList(node, last);
+	}
+
+	void ChainPendingNode(Node* node)
+	{
+		ChainPendingNodeList(node, node);
+	}
+
+	static void DeleteNodes(Node* node)
+	{
+		while (node)
+		{
+			Node* next = node->next;
+			delete node;
+			node = next;
+		}
+	}
 private:
-	atomic<Node*> _head; // 스택의 head를 원자적으로 관리합니다.
+	atomic<Node*> _head;
+
+
+	atomic<uint32> _popCount = 0; // Pop을 실행중인 쓰레드 개수를 저장하는 변수입니다.
+	atomic<Node*> _pendingList; // 삭제 되어야 할 노드들의 리스트를 저장하는 변수입니다.
 };
 
-/* 이 코드에서는 락-프리 스택을 구현했습니다. 락-프리 자료구조는 락을 사용하지 않고 동시성을 보장하는 자료구조입니다.
-이 경우에는 원자적 연산인 compare_exchange_weak를 사용하여 락-프리로 스택의 노드를 추가하고 추출하는 기능을 구현했습니다.
-락-프리 자료구조는 락을 사용하지 않기 때문에 경쟁 상태로 인한 성능 저하가 발생하지 않으며, 데드락이 발생하지 않는 이점이 있습니다. */
+/* 이 코드에서는 삭제해야 하는 노드들을 관리하는 방법이 추가되었습니다.
+Pop을 실행하는 스레드의 개수를 카운트하고, TryDelete 함수를 통해 삭제 대상 노드를 관리합니다.
+이를 통해 락-프리 스택에서 노드를 안전하게 삭제할 수 있습니다. 또한, ChainPendingNodeList와 ChainPendingNode 함수를 사용해
+삭제 대상 노드들을 연결하고 관리할 수 있습니다. 이를 통해 락-프리 스택에서 노드의 삭제를 효율적으로 처리할 수 있습니다. */
