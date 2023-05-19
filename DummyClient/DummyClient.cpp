98c08264978c5bf2dec22c14c632b7fc52bc9785
ejong2 <guyus5594@gmail.com>
2023-05-19 20:39:49 +0900

    [네트워크 프로그래밍] : 소켓 프로그래밍

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -1,7 +1,45 @@
 ﻿#include "pch.h"
 #include <iostream>
+#include <winsock2.h>  // Windows 전용 네트워크 프로그래밍 헤더
+#include <mswsock.h>  // Windows 소켓 API에 대한 Microsoft 전용 확장
+#include <ws2tcpip.h> // Winsock 함수, 구조체, 정의의 대부분을 포함
+#pragma comment(lib, "ws2_32.lib") // ws2_32 라이브러리 연결
 
 int main()
 {
-	HelloWorld();
+	WSAData wsaData; // 윈도우 소켓 정보를 담은 구조체
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) // 윈도우 소켓 초기화
+		return 0;
+
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0); // 클라이언트 소켓 생성 (IPv4, TCP)
+	if (clientSocket == INVALID_SOCKET) // 소켓 생성 실패 시
+	{
+		int32 errCode = ::WSAGetLastError(); // 에러 코드 추출
+		cout << "Socket ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	SOCKADDR_IN serverAddr; // 서버 주소를 담는 IPv4 구조체
+	::memset(&serverAddr, 0, sizeof(serverAddr)); // 주소 구조체 초기화
+	serverAddr.sin_family = AF_INET; // 주소 체계 지정 (IPv4)
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr); // 연결할 서버의 IP 주소 지정
+	serverAddr.sin_port = ::htons(7777); // 연결할 서버의 포트 번호 지정 (7777)
+
+	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) // 서버에 연결 요청
+	{
+		int32 errCode = ::WSAGetLastError(); // 에러 코드 추출
+		cout << "Connect ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	cout << "Connected To Server!" << endl; // 서버 연결 성공 메시지 출력
+	while (true) // 서버와 데이터 통신을 계속하기 위한 루프
+	{
+		// TODO: 서버와 데이터 통신 코드를 여기에 작성
+
+		this_thread::sleep_for(1s); // 현재 스레드를 1초 동안 중지 (추후 데이터 통신 코드 작성 후, 필요에 따라 수정)
+	}
+
+	::closesocket(clientSocket); // 클라이언트 소켓을 닫아 리소스 반환
+	::WSACleanup(); // 윈속 종료
 }
--- GameServer-Study/GameServer.cpp
+++ GameServer-Study/GameServer.cpp
@@ -6,104 +6,64 @@
 #include <windows.h>
 #include <future>
 #include "ThreadManager.h"
-
-#include "RefCounting.h"
-#include "Memory.h"
-#include "Allocator.h"
-#include "TypeCast.h"
-
-using TL = TypeList<class Player, class Mage, class Knight, class Archer>;
-
-class Player
-{
-public:
-	Player()
-	{
-		INIT_TL(Player);
-	}
-	virtual ~Player() {}
-
-	DECLARE_TL
-};
-
-class Knight : public Player
-{
-public:
-	Knight() { INIT_TL(Knight); }
-};
-
-class Mage : public Player
-{
-
-public:
-	Mage() { INIT_TL(Mage); }
-};
-
-class Archer : public Player
-{
-
-public:
-	Archer() { INIT_TL(Archer) }
-};
-
-class Dog
-{
-
-};
+#include <winsock2.h>  // Windows 전용 네트워크 프로그래밍 헤더
+#include <mswsock.h>  // Windows 소켓 API에 대한 Microsoft 전용 확장
+#include <ws2tcpip.h> // Winsock 함수, 구조체, 정의의 대부분을 포함
+#pragma comment(lib, "ws2_32.lib") // ws2_32 라이브러리 연결
 
 int main()
 {
-	//TypeList<Mage, Knight>::Head whoAMI;
-	//TypeList<Mage, Knight>::Tail whoAMI2;
+	WSAData wsaData; // 윈도우 소켓 정보를 담은 구조체
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) // 윈도우 소켓 초기화
+		return 0;
 
-	//TypeList<Mage, TypeList<Knight, Archer>>::Head whoAMI3;
-	//TypeList<Mage, TypeList<Knight, Archer>>::Tail::Head whoAMI4;
-	//TypeList<Mage, TypeList<Knight, Archer>>::Tail::Tail whoAMI5;
-
-	//int32 len1 = Length<TypeList<Mage, Knight>>::value;
-	//int32 len2 = Length<TypeList<Mage, Knight, Archer>>::value;
-	//
-	//
-	//// 3*3 
-	//TypeAt<TL, 0>::Result whoAMI6;
-	//TypeAt<TL, 1>::Result whoAMI7;
-	//TypeAt<TL, 2>::Result whoAMI8;
-
-	//int32 index1 = IndexOf<TL, Mage>::value;
-	//int32 index2 = IndexOf<TL, Archer>::value;
-	//int32 index3 = IndexOf<TL, Dog>::value;
-
-	//bool canConvert1 = Conversion<Player, Knight>::exists;
-	//bool canConvert2 = Conversion<Knight, Player>::exists;
-	//bool canConvert3 = Conversion<Knight, Dog>::exists;
-
-	/*{
-		Player* player = new Knight();
-
-		bool canCast = CanCast<Knight*>(player);
-		Knight* knight = TypeCast<Knight*>(player);
-
-
-		delete player;
-	}*/
-
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0); // 서버 소켓 생성 (IPv4, TCP)
+	if (listenSocket == INVALID_SOCKET) // 소켓 생성 실패 시
 	{
-		shared_ptr<Player> player = MakeShared<Knight>();
-
-		shared_ptr<Archer> archer = TypeCast<Archer>(player);
-		bool canCast = CanCast<Mage>(player);
-
+		int32 errCode = ::WSAGetLastError(); // 에러 코드 추출
+		cout << "Socket ErrorCode : " << errCode << endl;
+		return 0;
 	}
 
-	for (int32 i = 0; i < 5; i++)
+	SOCKADDR_IN serverAddr; // 서버 주소를 담는 IPv4 구조체
+	::memset(&serverAddr, 0, sizeof(serverAddr)); // 주소 구조체 초기화
+	serverAddr.sin_family = AF_INET; // 주소 체계 지정 (IPv4)
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY); // IP 주소는 시스템에 의해 자동 설정
+	serverAddr.sin_port = ::htons(7777); // 포트 번호 지정 (7777)
+
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) // 서버 소켓에 주소 바인드
 	{
-		GThreadManager->Launch([]()
-			{
-				while (true)
-				{
-				}
-			});
+		int32 errCode = ::WSAGetLastError(); // 에러 코드 추출
+		cout << "Bind ErrorCode : " << errCode << endl;
+		return 0;
 	}
 
-	GThreadManager->Join();
+	if (::listen(listenSocket, 10) == SOCKET_ERROR) // 서버 소켓을 리스닝 상태로 변경 (백로그 큐 크기 10)
+	{
+		int32 errCode = ::WSAGetLastError(); // 에러 코드 추출
+		cout << "Listen ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	while (true) // 클라이언트의 연결 요청을 계속 기다림
+	{
+		SOCKADDR_IN clientAddr; // 클라이언트 주소를 담는 IPv4 구조체
+		::memset(&clientAddr, 0, sizeof(clientAddr)); // 주소 구조체 초기화
+		int32 addrLen = sizeof(clientAddr);
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen); // 클라이언트의 연결 요청 수락
+		if (clientSocket == INVALID_SOCKET) // 연결 수락 실패 시
+		{
+			int32 errCode = ::WSAGetLastError(); // 에러 코드 추출
+			cout << "Accept ErrorCode : " << errCode << endl;
+			return 0;
+		}
+
+		char ipAddress[16]; // IP 주소를 담을 배열
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress)); // 클라이언트의 IP 주소를 문자열로 변환
+		cout << "Client Connected! IP = " << ipAddress << endl; // 연결된 클라이언트의 IP 주소 출력
+
+		// TODO: 클라이언트와 데이터 통신 코드를 여기에 작성
+	}
+
+	::WSACleanup(); // 윈속 종료
 }
