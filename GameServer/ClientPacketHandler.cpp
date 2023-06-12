4d9e21558b86808f25732660e875098f56423778
ejong2 <guyus5594@gmail.com>
2023-06-12 11:15:46 +0900

    [JobQueue] : JobQueue #2

--- GameServer/ClientPacketHandler.cpp
+++ GameServer/ClientPacketHandler.cpp
@@ -74,7 +74,8 @@ bool Handle_C_ENTER_GAME(PacketSessionRef& session, Protocol::C_ENTER_GAME& pkt)
 
 	PlayerRef player = gameSession->_players[index]; // READ_ONLY?
 
-	GRoom.PushJob(MakeShared<EnterJob>(GRoom, player));
+	//GRoom.PushJob(MakeShared<EnterJob>(GRoom, player));
+	GRoom.PushJob(&Room::Enter, player);
 
 	Protocol::S_ENTER_GAME enterGamePkt;
 	enterGamePkt.set_success(true);
@@ -92,7 +93,8 @@ bool Handle_C_CHAT(PacketSessionRef& session, Protocol::C_CHAT& pkt)
 	chatPkt.set_msg(pkt.msg());
 	auto sendBuffer = ClientPacketHandler::MakeSendBuffer(chatPkt);
 
-	GRoom.PushJob(MakeShared<BroadcastJob>(GRoom, sendBuffer));
+	//GRoom.PushJob(MakeShared<BroadcastJob>(GRoom, sendBuffer));
+	GRoom.PushJob(&Room::Broadcast, sendBuffer);
 
 	return true;
 }
