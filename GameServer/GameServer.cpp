e4fd6494f22ab6a78bbf6204ea087b10914fb71f
ejong2 <guyus5594@gmail.com>
2023-06-14 10:13:32 +0900

    [JobQueue] : JobQueue #5

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -12,28 +12,47 @@
 #include "Room.h"
 #include "Player.h"
 
+enum
+{
+    WORKER_TICK = 64
+};
+
+void DoWorkerJob(ServerServiceRef& service)
+{
+    while (true)
+    {
+        LEndTickCount = ::GetTickCount64() + WORKER_TICK;
+
+        // 네트워크 입출력 처리 -> 인게임 로직까지 (패킷 핸들러에 의해)
+        service->GetIocpCore()->Dispatch(10);
+
+        // 글로벌 큐
+        ThreadManager::DoGlobalQueueWork();
+    }
+}
+
 int main()
 {
-	ClientPacketHandler::Init();
+    ClientPacketHandler::Init();
 
-	ServerServiceRef service = MakeShared<ServerService>(
-		NetAddress(L"127.0.0.1", 7777),
-		MakeShared<IocpCore>(),
-		MakeShared<GameSession>, // TODO : SessionManager 등
-		100);
+    ServerServiceRef service = MakeShared<ServerService>(
+        NetAddress(L"127.0.0.1", 7777),
+        MakeShared<IocpCore>(),
+        MakeShared<GameSession>, // TODO : SessionManager 등
+        100);
 
-	ASSERT_CRASH(service->Start());
+    ASSERT_CRASH(service->Start());
 
-	for (int32 i = 0; i < 5; i++)
-	{
-		GThreadManager->Launch([=]()
-			{
-				while (true)
-				{
-					service->GetIocpCore()->Dispatch();
-				}				
-			});
-	}
+    for (int32 i = 0; i < 5; i++)
+    {
+        GThreadManager->Launch([&service]()
+            {
+                DoWorkerJob(service);
+            });
+    }
 
-	GThreadManager->Join();
+    // Main Thread
+    DoWorkerJob(service);
+
+    GThreadManager->Join();
 }
